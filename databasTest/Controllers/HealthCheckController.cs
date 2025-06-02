9a962f01468103d9ec7b1f4462afc183e087aa3a
StrStark <rezafathisamani1383@gmail.com>
2025-06-02 10:21:56 +0330

    health check added

---
+++ databasTest/Controllers/HealthCheckController.cs
@@ -0,0 +1,26 @@
+﻿using databasTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace databasTest.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly AdventureWorks2022Context _context;
+
+    public HealthCheckController(AdventureWorks2022Context context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("db")]
+    public async Task<IActionResult> CheckDatabaseConnection()
+    {
+        bool canConnect = await _context.Database.CanConnectAsync();
+        if (canConnect)
+            return Ok(new { status = "Healthy", db = "Connected" });
+        else
+            return StatusCode(500, new { status = "Unhealthy", db = "Cannot connect" });
+    }
+}
---
+++ databasTest/DatabaseHealthService.cs
@@ -0,0 +1,18 @@
+﻿using databasTest.Models;
+
+namespace databasTest;
+
+public class DatabaseHealthService
+{
+    private readonly AdventureWorks2022Context _context;
+
+    public DatabaseHealthService(AdventureWorks2022Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CanConnectAsync()
+    {
+        return await _context.Database.CanConnectAsync();
+    }
+}
